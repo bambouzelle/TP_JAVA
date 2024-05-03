a734f6017a8121f4938082ff112004f2a4fa17c7
Félix BERNARD <felix.bernard@epsi.fr>
2024-05-03 16:37:49 +0200

    avec plus de détail

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,12 @@ public class Main {
         byte octets = 126;
         short oui = 1;
         int nombre = 1561321;
-        long oui2 = 4894894;
-        float oui3= 456456;
-        double oui4= 456456;
+        long oui2 = 4894894L;
+        float oui3= 456456.3F;
+        double oui4= 456456.12;
         boolean oui5= true;
         char oui6= 'a';
         String oui7= "hello world";
-
-
 
         System.out.println(oui7);
     }
