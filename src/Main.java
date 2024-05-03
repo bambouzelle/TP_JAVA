e2fda52132e3465a68e36e0145957448f89d569e
Félix BERNARD <felix.bernard@epsi.fr>
2024-05-03 15:52:43 +0200

    TP2 affichage d'une valeur

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        byte octets = 126;
+        short oui = 1;
+        int nombre = 1561321;
+        long oui2 = 4894894;
+        float oui3= 456456;
+        double oui4= 456456;
+        boolean oui5= true;
+        char oui6= 'a';
+        String oui7= "hello world";
+
+
+
+        System.out.println(oui7);
+    }
+}
